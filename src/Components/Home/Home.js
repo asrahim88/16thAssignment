9d2ff6e8bfeac086cd57383e8f4325bb7377e8ab
asrahim88 <tasmimhasan1997@gmail.com>
2021-06-09 21:23:24 +0600

    updated

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Header from '../Header/Header';
+import NavBar from '../NavBar/NavBar';
+
+
+const Home = () => {
+    return (
+       <NavBar></NavBar>
+    //   <Header></Header>
+       
+    );
+};
+
+export default Home;
